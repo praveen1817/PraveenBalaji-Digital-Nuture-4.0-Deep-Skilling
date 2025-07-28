794af9bc8f3acd5cf24ec6e1613924bcdb517132
praveen1817 <praveen42165@gmail.com>
2025-07-28 12:23:25 +0530

    Add files via upload

---
+++ DeepSkilling/Week-7/ReactJS-HQL10/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import officeImage from './office1.jpeg';
+
+
+function App() {
+  const element = "Office Space";
+
+  const jsxatt = <img src={officeImage} width="25%" height="25%" alt="Office Space" />;
+
+ 
+  const ItemName = {
+    Name: "MMS",
+    Rent: 70000,
+    Address: "Chennai"
+  };
+
+  let colors = [];
+  if (ItemName.Rent <= 60000) {
+    colors.push("textRed");
+  } else {
+    colors.push("textGreen");
+  }
+
+  return (
+    <div className="App">
+      <h1>{element} , at Affordable Range</h1>
+      {jsxatt}
+
+      <h1>Name: {ItemName.Name}</h1>
+      <h3 className={colors.join(" ")}>Rent: Rs. {ItemName.Rent}</h3>
+      <h3>Address: {ItemName.Address}</h3>
+    </div>
+  );
+}
+
+export default App;
